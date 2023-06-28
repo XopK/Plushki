ca60e507137dc42c4708a3cf5250eae7f230e557
XopK <dmahmutov12@gmail.com>
2023-06-28 16:49:32 +0500

    trtrrt

--- login.php
+++ login.php
@@ -8,7 +8,7 @@ session_start();
         <h2>Авторизация</h2>
         <form action="/login.php" method="POST">
             <input type="email" placeholder="Почта пользователя" name="Email" required>
-            <input type="password" placeholder="Пароль" name="password" pattern=".{8,} required>
+            <input type="password" placeholder="Пароль" name="password" pattern=".{8,}" required>
             <button type="submit" style = "margin-bottom:10px;">Войти</button>
         </form>
         <a href="/registration.php">Регистрация</a>
